83fccb632def2a59fafa45043c09ab2358bdaa95
Joseph.Khajo <Joseph.Khajo@sksoldev.com>
2025-04-24 15:07:54 +0400

    Added logic for matching patterns

--- gogrep.go
+++ gogrep.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	// "io"
-	"os"
-	"log"
 	"fmt"
+	"log"
+	"os"
+	"strings"
+	"github.com/fatih/color"
 )
 
 func getFileSize(filename string) int64 {
@@ -15,13 +16,35 @@ func getFileSize(filename string) int64 {
 	return fi.Size()
 }
 
+func matchString(haystack, needle string) {
+	if needle == "" {
+		fmt.Println("No string to match")
+		return
+	}
+	if len(needle) > len(haystack) {
+		fmt.Println("String to match is longer than the haystack")
+		return
+	}
+
+	index := strings.Index(strings.ToLower(haystack), strings.ToLower(needle))
+	for index != -1 {
+		fmt.Print(color.YellowString(haystack[:index]))
+		fmt.Print(color.GreenString(haystack[index:index+len(needle)]))
+		haystack = haystack[index+len(needle):]
+		index = strings.Index(strings.ToLower(haystack), strings.ToLower(needle))
+	}
+	fmt.Println(color.YellowString(haystack))
+}
+
 func main() {
 	args := os.Args
-	if len(args) < 2 {
-		log.Fatal("Please provide a filename")
+	if len(args) < 3 {
+		log.Fatal("./gogrep <filename> <string-to-match>")
 		return
 	}
 	filename := args[1]
+	to_match := args[2]
+
 	size := getFileSize(filename)
 	data := make([]byte, size)
 	file, err := os.Open(filename)
@@ -31,11 +54,8 @@ func main() {
 	}
 	defer file.Close()
 
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Println("Read", count, "bytes")
-	fmt.Println(string(data[:count]))
+	file.Read(data)
+	string_data := string(data)
+	matchString(string_data, to_match)
+	tui_test()
 }
